26e06dbb2cb721369272384dd44fb0ee06c9b855
Akshaj77 <cse21008@gmail.com>
2024-05-27 11:29:11 +0530

    views-recommend-posts

--- recommend_post/views.py
+++ recommend_post/views.py
@@ -7,6 +7,7 @@ from surprise import Dataset, Reader, SVD
 import pandas as pd
 from user.models import User
 from django.http import HttpRequest
+import traceback
 
 
 def generate_interaction_matrix():
@@ -26,7 +27,7 @@ def generate_interaction_matrix():
         data.append([save["user_id"], save["post_id"], 3])
 
     df = pd.DataFrame(data, columns=["user_id", "post_id", "interaction"])
-    print(df)
+    # print(df)
 
     interaction_matrix = df.pivot_table(
         index="user_id",
@@ -53,17 +54,24 @@ def train_model(trainset):
     return model
 
 
-def recommend_posts(model, user_id, interaction_matrix, top_n=10):
-    user_ratings = interaction_matrix.loc[str(user_id)].values
-    user_interactions = {
-        post_id: rating for post_id, rating in enumerate(user_ratings) if rating != 0
-    }
+def recommend_posts(model, user_id, interaction_matrix, top_n=2):
+    # user_id = '74g82gOrwYRAMd5tu56LQg8pQEs2'  # replace with the actual user_id
+    user_ratings = interaction_matrix.loc[user_id]
+    user_interactions = {post_id: rating for post_id, rating in user_ratings.items() if rating != 0}
+    # user_ratings = interaction_matrix.loc[str(user_id)].values
+    # print(user_ratings)
+    # user_interactions = {
+    #     post_id : rating for post_id, rating in enumerate(user_ratings) if rating != 0
+    # }
 
+    print("columns")
+    print(interaction_matrix.columns)
+    print(user_interactions)
     all_post_ids = set(interaction_matrix.columns)
     seen_post_ids = set(user_interactions.keys())
     unseen_post_ids = list(all_post_ids - seen_post_ids)
 
-    predictions = [model.predict(user_id, post_id) for post_id in unseen_post_ids]
+    predictions = [model.predict(uid=user_id, iid=post_id) for post_id in unseen_post_ids]
     recommendations = sorted(predictions, key=lambda x: x.est, reverse=True)[:top_n]
 
     return [rec.iid for rec in recommendations]
@@ -73,8 +81,9 @@ def recommend_posts(model, user_id, interaction_matrix, top_n=10):
 # @permission_classes([IsAuthenticated])
 def get_recommended_posts(request):
     user = User.objects.all().first()
-    print(user.uid)
-    print(user.email)
+    # print(user)
+    # print(user.uid)
+    # print(user.email)
     interaction_matrix = generate_interaction_matrix()
     trainset = prepare_data(interaction_matrix)
     model = train_model(trainset)
@@ -83,7 +92,7 @@ def get_recommended_posts(request):
         recommended_posts = CommunityPost.objects.filter(id__in=recommended_post_ids)
 
         fake_request = HttpRequest()
-        fake_request.user = "74g82gOrwYRAMd5tu56LQg8pQEs2"
+        fake_request.user = "QH98P2w9lxXVl5ZG7rvviAZ23X32"
 
         serializer = CommunityPostListSerializer(
             recommended_posts,
@@ -94,4 +103,5 @@ def get_recommended_posts(request):
     except Exception as e:
         # Handle the exception here
         print(f"An error occurred: {str(e)}")
+        # traceback.print_exc()
         return Response([])
